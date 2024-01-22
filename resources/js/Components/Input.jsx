fb14417c1197e29087936a17558c8c058a55b4c3
rudiarip <rudiariprayogi@gmail.com>
2024-01-22 21:08:25 +0700

    feat:instal breeze

---
+++ resources/js/Components/Input.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useRef} from "react";
+
+export default function Input({
+    type = 'text',
+    name,
+    value,
+    defaultValue,
+    className,
+    variant = "primary",
+    autoComplete,
+    required,
+    isFocused,
+    handleChange,
+    placeholder,
+    isError,
+}){
+    const input = useRef();
+
+    useEffect(() => {
+        if(isFocused){
+            input.current.focus();
+        }
+    }, []);
+
+    return(
+        <div className="flex flex-col items-start">
+            <input 
+            type={type}
+            name={name}
+            value={value}
+            className={'rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none'}
+            ref={input}
+            autoComplete={autoComplete}
+            required={required}
+            onChange={(e) => handleChange(e)}
+            placeholder={placeholder}
+            />
+        </div>
+    );
+
+}
